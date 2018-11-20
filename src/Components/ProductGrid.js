fd777bfe8c26852fa21a061f3a95f9fdc677894a
Jessica Skok <jessica.skok@gmail.com>
2018-11-19 21:57:43 -0800

    add images and libraries for styling out grid

--- src/Components/ProductGrid.js
+++ src/Components/ProductGrid.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Products from '../products.json';
+import Img from 'react-image';
+import fallBack from '../Assets/no-image.jpg'
 
 class ProductGrid extends Component {
+
   render() {
     return (
       <div>
@@ -9,7 +12,11 @@ class ProductGrid extends Component {
        {Products.documents.map((productDetail, index)=>{
          return <div> 
            <h1>{productDetail.productNo}</h1>
-           <img src={productDetail.images[1]}/>
+           <Img src={
+             [productDetail.images[1],
+             'http://sanpai-japan.com/wp-content/uploads/2017/06/no-image-icon-24.jpg']
+            }
+            />
            <p>{productDetail.skuDisplayName_en[0]}</p>
          </div>
        })}
